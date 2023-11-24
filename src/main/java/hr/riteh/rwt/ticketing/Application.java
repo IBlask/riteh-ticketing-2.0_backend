39b500d6b74e30c95a79e3365b0ea924f3e71566
IBlask <95757169+IBlask@users.noreply.github.com>
2023-11-24 13:45:45 +0100

    Init

---
+++ src/main/java/hr/riteh/rwt/ticketing/Application.java
@@ -0,0 +1,13 @@
+package hr.riteh.rwt.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
